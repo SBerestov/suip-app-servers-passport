f8ba1beb91c82abe07745172a46794145d9ac47a
Sasa soska <s08082003a@gmail.com>
2025-09-20 22:45:02 +0500

    Deploy to railway

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,16 +1,27 @@
-from flask import Flask
+import os
+from flask import Flask, send_from_directory
 from flask_cors import CORS
 from config import Config
-from .routes.main import main_bp
 from .routes.api import api_bp
 
 def create_app():
-    app = Flask(__name__)
+    static_folder = os.path.join(os.path.dirname(__file__), "../frontend/dist")
+    app = Flask(__name__, static_folder=static_folder, static_url_path="/")
     app.config.from_object(Config)
-    
-    CORS(api_bp, origins="http://localhost:3000")
-    
-    app.register_blueprint(main_bp)
-    app.register_blueprint(api_bp, url_prefix='/api')
-    
+
+    FRONTEND_URL = os.getenv('FRONTEND_URL')
+    if FRONTEND_URL:
+        CORS(app, resources={r"/api/*": {"origins": FRONTEND_URL}})
+    else:
+        CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+    app.register_blueprint(api_bp, url_prefix="/api")
+
+    @app.route("/", defaults={"path": ""})
+    @app.route("/<path:path>")
+    def serve_react(path):
+        if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+            return send_from_directory(app.static_folder, path)
+        return send_from_directory(app.static_folder, "index.html")
+
     return app
